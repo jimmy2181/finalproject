947d56577c65253a12e59e71b19e75d1cce31be9
SkynetDev <andrejohnson0017@gmail.com>
2019-07-11 21:45:26 -0400

    updated stock structure

--- models/stock.js
+++ models/stock.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
+const stockSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
   synopsis: String,
   date: { type: Date, default: Date.now }
 });
 
-const Book = mongoose.model("Book", bookSchema);
+const Stock = mongoose.model("Stock", stockSchema);
 
-module.exports = Book;
+module.exports = Stock;
